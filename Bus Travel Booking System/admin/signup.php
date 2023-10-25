9dafcb278076f02dc63129b56f95708e0c598ca1
KAVIYA S <125296317+kaviya-Srii@users.noreply.github.com>
2023-10-25 20:08:08 +0530

    Update signup.php

--- Bus Travel Booking System/admin/signup.php
+++ Bus Travel Booking System/admin/signup.php
@@ -1,4 +1,3 @@
-<!-- Show these admin pages only when the admin is logged in -->
 <?php  require '../assets/partials/_admin-check.php';   ?>
 
 <!DOCTYPE html>
@@ -77,7 +76,6 @@
         </div>
     <script src="../assets/scripts/admin_signup.js">
     </script>
-    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>  
 </body>
 </html>
